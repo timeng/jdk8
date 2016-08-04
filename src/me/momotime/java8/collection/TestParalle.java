85769f9c52c62c6a8de7755c52cfac5581f34644
timeng <timengsz@gmail.com>
2016-08-04 19:58:04 +0800

    重新规划项目结构

---
+++ src/me/momotime/java8/collection/TestParalle.java
@@ -0,0 +1,28 @@
+package me.momotime.java8.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Paralle API
+ * 
+ * @author TM
+ * @since 2016/8/4 000419:38
+ */
+public class TestParalle {
+
+    public static void main(String args[]) {
+        List list = new ArrayList<Object>();
+        for (int i1 = 0; i1 < 1000; i1++) {
+            list.add(i1);
+        }
+
+        Predicate<Object> filteredPredicate = (Object t) -> (t != null);
+        /**
+         * filter API
+         */
+        List list1 = (List) list.stream().filter(filteredPredicate).collect(Collectors.toList());
+    }
+}
---
+++ src/me/momotime/java8/collection/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 集合框架并行计算API
+ */
+package me.momotime.java8.collection;
